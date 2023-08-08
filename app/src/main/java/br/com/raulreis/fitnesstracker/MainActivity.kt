1adffbbc75c9433924baefc0950bf50009bbae18
Raul Reis <raul.soares@usp.br>
2023-08-07 21:07:59 -0300

    Ajustes de pacote no arquivo Main e inclusão da dependência do Room para manipulação de BD

--- app/src/main/java/br/com/raulreis/fitnesstracker/MainActivity.kt
+++ app/src/main/java/br/com/raulreis/fitnesstracker/MainActivity.kt
@@ -1,17 +1,16 @@
 package br.com.raulreis.fitnesstracker
 
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -34,25 +33,39 @@ class MainActivity : AppCompatActivity() {
         mainItems.add(
             MainItem(
                 id = 2,
-                drawableID = R.drawable.baseline_balance_24,
+                drawableID = R.drawable.baseline_monitor_heart_24,
                 textStringId = R.string.label_bmr,
                 color = Color.YELLOW
             )
         )
 
-        val adapter = MainAdapter(mainItems)
+        val adapter = MainAdapter(mainItems) { id ->
+            when (id) {
+                1 -> {
+                    val intent = Intent(this@MainActivity, BmiActivity::class.java)
+                    startActivity(intent)
+                }
+
+                2 -> {
+                    // Abrir outra activity
+                }
+            }
+        }
         rvMain = findViewById(R.id.rvMainButtons)
         rvMain.adapter = adapter
 
         rvMain.layoutManager = GridLayoutManager(this, 2)
 
-        // Calsse para administrar a recyclerview e suas celulas (e os seus layouts de itens)
+        // Classe para administrar a recyclerview e suas celulas (e os seus layouts de itens)
         // Adapter ->
 
 
     }
 
-    private inner class MainAdapter(private val mainItems : List<MainItem>): RecyclerView.Adapter<MainViewHolder>() {
+    private inner class MainAdapter(
+        private val mainItems : List<MainItem>,
+        private val onItemClickListener: (Int) -> Unit
+    ): RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
         // 1 - Imforma qual é o layout XML da celula específica (item)
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
             val view = layoutInflater.inflate(R.layout.main_item, parent, false)
@@ -70,18 +83,26 @@ class MainActivity : AppCompatActivity() {
             val itemCurrent = mainItems[position]
             holder.bind(itemCurrent)
         }
+
+        private inner class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind(item: MainItem) {
+                val img: ImageView = itemView.findViewById(R.id.imgItemIcon)
+                val name: TextView = itemView.findViewById(R.id.txtItemName)
+                val container : LinearLayout = itemView.findViewById(R.id.containerItemBmi)
+
+                img.setImageResource(item.drawableID)
+                name.setText(item.textStringId)
+                container.setBackgroundColor(item.color)
+
+                container.setOnClickListener {
+                    onItemClickListener.invoke(item.id)
+                }
+            }
+        }
     }
 
     //Classe da celula em si
-    private class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(item: MainItem) {
-            val img: ImageView = itemView.findViewById(R.id.imgItemIcon)
-            val name: TextView = itemView.findViewById(R.id.txtItemName)
-            val container : LinearLayout = itemView.findViewById(R.id.containerItemBmi)
 
-            img.setImageResource(item.drawableID)
-            name.setText(item.textStringId)
-            container.setBackgroundColor(item.color)
-        }
-    }
+
+
 }
